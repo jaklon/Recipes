6ce771d79730a5e58fc5419b1d3b5cacfe7a5023
jaklon <jasconkembuan9@gmail.com>
2023-12-11 16:03:19 +0700

    fifth commit

--- app/src/main/java/com/example/recipesapp/fragment/CategoriesFragment.java
+++ app/src/main/java/com/example/recipesapp/fragment/CategoriesFragment.java
@@ -6,19 +6,44 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.recipes.databinding.FragmentCategoryBinding;
+import com.example.recipesapp.adapters.CategoryAdapter;
+import com.example.recipesapp.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoriesFragment extends Fragment {
 
     private FragmentCategoryBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentCategoryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loadCategories();
+    }
+
+    private void loadCategories() {
+        binding.rvCategories.setAdapter(new CategoryAdapter());
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("1", "Breakfast",""));
+        categories.add(new Category("2", "Lunch",""));
+        categories.add(new Category("2", "Dinner",""));
+        CategoryAdapter adapter = (CategoryAdapter) binding.rvCategories.getAdapter();
+        if (adapter != null) {
+            adapter.setCategoryList(categories);
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
